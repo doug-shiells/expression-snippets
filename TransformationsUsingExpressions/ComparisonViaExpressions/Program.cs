64928ba47d81257477855c1bbdf4f576e7213731
Doug Shiells <dougshiells@gamail.com>
2019-08-24 12:54:17 +1000

    Transforming and equality checking dynamic objects using expressions

---
+++ TransformationsUsingExpressions/ComparisonViaExpressions/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ComparisonViaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            dynamic[] left = new dynamic[] {
+            new {Name = "John", Age = 53, fk = 12},
+            new {Name = "Alex", Age = 22, fk = 12},
+            new {Name = "Angela", Age = 33, fk = 12}
+            };
+
+                    dynamic[] right = new dynamic[] {
+            new {Name = "John", Age = 53, fk = 1},
+            new {Name = "Alex", Age = 21, fk = 12},
+            new {Name = "Angela", Age = 33, fk = 12}
+            };
+
+            //Used for type identification in generic method
+            var dummySourceInstance = left[0];
+            var dynamicEqualityComparer = ExpressionHelpers.True(dummySourceInstance);
+            foreach (var property in dummySourceInstance.GetType().GetProperties())
+            {
+                //Used for type identification in generic method
+                var dummyPropertyInstance = property.GetValue(dummySourceInstance);
+
+                //expression to read property from object
+                var propertyExpression = ExpressionHelpers.GetPropertyExpression(dummySourceInstance, dummyPropertyInstance, property.Name);
+
+                //expression to transform right property if foreign key
+                var transformedPropertyExpression = ExpressionHelpers.GetTransformation(propertyExpression, property.Name);
+
+                //build expression equivilant to propertyExpression(parameter1) == propertyExpression(parameter2)
+                var equalityExpression = ExpressionHelpers.AreEqual(propertyExpression, transformedPropertyExpression);
+
+                //append expression to existing expressiont tree with and operator
+                dynamicEqualityComparer = ExpressionHelpers.And(dynamicEqualityComparer, equalityExpression);
+            }
+
+            //Compile expression tree
+            var compiledExpression = dynamicEqualityComparer.Compile();
+
+            //Execute expression over data set 
+            for (int i = 0; i < left.Length; i++)
+            {
+                Console.WriteLine(compiledExpression(left[i], right[i]));
+            }
+        }
+
+    }
+    public static class ForeignKeyTransformer
+    {
+        public static string[] foreignKeys = new string[] { "fk" };
+        public static int TransformForeignKey(int key, string propertyName)
+        {
+            if (foreignKeys.Contains(propertyName))
+            {
+                Console.WriteLine("Mapping key for column " + propertyName);
+                return 12; //Do actual mapping transformation here
+            }
+            else
+                return key;
+        }
+    }
+
+    public static class ExpressionHelpers
+    {
+        public static Expression<Func<T, T, bool>> True<T>(T source) => (l, r) => true;
+
+        public static Expression<Func<T, T, bool>> AreEqual<T, T2>(Expression<Func<T, T2>> left, Expression<Func<T, T2>> right)
+        {
+            var leftParam = Expression.Parameter(typeof(T));
+            var rightParam = Expression.Parameter(typeof(T));
+
+            var leftInvoked = Expression.Invoke(left, leftParam);
+            var rightInvoked = Expression.Invoke(right, rightParam);
+
+            return Expression.Lambda<Func<T, T, bool>>(Expression.Equal(leftInvoked, rightInvoked), new ParameterExpression[] { leftParam, rightParam });
+        }
+
+        public static Expression<Func<T, T2>> GetPropertyExpression<T, T2>(T source, T2 property, string propertyName)
+        {
+            var p = Expression.Parameter(typeof(T));
+            var propertyReaderExp = Expression.PropertyOrField(p, propertyName);
+
+            return Expression.Lambda<Func<T, T2>>(propertyReaderExp, p);
+        }
+
+        public static Expression<Func<T, T2>> GetTransformation<T, T2>(Expression<Func<T, T2>> expr, string propertyName)
+        {
+            if (typeof(T2) == typeof(int))
+            {
+                var propertyNameExpression = Expression.Constant(propertyName);
+                var propertyValueExpressionParameter = Expression.Parameter(typeof(T));
+
+                var invokedExpression = Expression.Invoke(expr, propertyValueExpressionParameter);
+
+                var methodCallingExpression = Expression.Call(typeof(ForeignKeyTransformer).GetMethod("TransformForeignKey"), invokedExpression, propertyNameExpression);
+                return Expression.Lambda<Func<T, T2>>(methodCallingExpression, propertyValueExpressionParameter);
+            }
+            return expr;
+        }
+
+        public static Expression<Func<T, T, bool>> And<T>(Expression<Func<T, T, bool>> expr1,
+                                                         Expression<Func<T, T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, T, bool>>
+                  (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+    }
+}
---
+++ TransformationsUsingExpressions/TransformationsUsingExpressions/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace TransformationsUsingExpressions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //get source data
+            dynamic[] sourceData = new dynamic[] {
+            new {Name = "John", Age = 53, fk = 1},
+            new {Name = "Alex", Age = 22, fk = 2},
+            new {Name = "Angela", Age = 33, fk = 3},
+            new {Name = "Angela", Age = 33, fk = 4}
+            };
+            //create example object for type resolution in generic classes
+            dynamic example = sourceData[0];
+            //Compile transformation expression
+            dynamic transformer = Transformer(example);
+            //transform data
+            var transformed = sourceData.Select(x => transformer(x));
+
+            //print results
+            var enumerator = transformed.GetEnumerator();
+            foreach(var source in sourceData)
+            {
+                enumerator.MoveNext();
+                Console.WriteLine($"{source.Name} => {enumerator.Current.Name}");
+                Console.WriteLine($"{source.Age} => {enumerator.Current.Name}");
+                Console.WriteLine($"{source.fk} => {enumerator.Current.fk}");
+                Console.WriteLine($"~~~~~~~~~~~~~~~~~~~~~");
+            }
+        }
+        
+
+        /// <summary>
+        /// Generates a delegate that accepts a source object 
+        /// and returns a clone with foreign key mappings applied
+        /// </summary>
+        public static Func<T, T> Transformer<T>(T example)
+        {
+            var type = typeof(T);
+            var parentParameter = Expression.Parameter(typeof(T));
+            //Get first constructor
+            var ctor = type.GetConstructors()[0];
+            //Get parameters for contstructor
+            var ctorParams = ctor.GetParameters();
+            //Construct expressions to resolve each parameter from the source object and apply foreign key mapping if applicable
+            var parameterExpressions = ctorParams.Select(parameter => ApplyForeignKeyMap(example, parameter, parentParameter));
+            //Build expression to execute the constructor 
+            var constructorExpression = Expression.New(ctor, parameterExpressions);
+            //Compile expression to a func<T,T>
+            return Expression.Lambda<Func<T,T>>(constructorExpression, parentParameter).Compile();
+        }
+
+        /// <summary>
+        /// Returns an expression that reads a parameter from the source object and if applicable, applies the foreign key map
+        /// </summary>
+        private static InvocationExpression ApplyForeignKeyMap<T>(T example, ParameterInfo parameter, ParameterExpression parentParameter)
+        {
+            return ForeignKeyTransformer.IsForeignKey(parameter.Name)
+                //Convert to invocation expression (I am calling map foreign key)
+                ? Expression.Invoke(
+                    //Build expression to call MapForeignKey method with argument parameter 
+                    ExpressionHelpers.GenerateForeignKeyTransformation(
+                        //Build expression to read parameter from source object
+                        ExpressionHelpers.GetProperty(example, (dynamic)typeof(T).GetProperty(parameter.Name).GetValue(example), parameter.Name), parameter.Name),
+                    parentParameter)
+                //Convert to invocation expression (I am calling the parameters get method)
+                : Expression.Invoke(
+                    //Build expression to read parameter from source object
+                    ExpressionHelpers.GetProperty(example, (dynamic)typeof(T).GetProperty(parameter.Name).GetValue(example), parameter.Name),
+                    parentParameter);
+        }
+    }
+
+    public static class ForeignKeyTransformer
+    {
+        public static string[] foreignKeyColumns = new string[] { "fk" };
+        public static Dictionary<string, Dictionary<dynamic, dynamic>> ForeignKeyMap;
+
+        static ForeignKeyTransformer()
+        {
+            ForeignKeyMap = PopulateForeginKeyMap();
+        }
+
+        public static T TransformForeignKey<T>(T key, string propertyName)
+        {
+            return ForeignKeyMap[propertyName][key]; 
+        }
+
+        public static bool IsForeignKey(string propertyName)
+        {
+            return foreignKeyColumns.Contains(propertyName);
+        }
+
+        private static Dictionary<string, Dictionary<dynamic, dynamic>> PopulateForeginKeyMap()
+        {
+            var map = new Dictionary<string, Dictionary<dynamic, dynamic>>();
+            map.Add("fk", new Dictionary<dynamic, dynamic>());
+            map["fk"].Add(1, 12);
+            map["fk"].Add(2, 13);
+            map["fk"].Add(3, 14);
+            map["fk"].Add(4, 15);
+            return map;
+        }
+    }
+
+
+    public static class ExpressionHelpers
+    {
+        /// <summary>
+        /// Returns an expression that will read a property with a given name from a source object
+        /// </summary>
+        /// <typeparam name="T">Type of the source object</typeparam>
+        /// <typeparam name="T2">Type of the property to be read</typeparam>
+        /// <param name="sourceExample">Example object used to resolve Generic type T</param>
+        /// <param name="propertyExample">Example object used to resolve Generic type T2</param>
+        /// <param name="propertyName">Name of the property to be read</param>
+        /// <returns></returns>
+        public static Expression<Func<T, T2>> GetProperty<T, T2>(T sourceExample, T2 propertyExample, string propertyName)
+        {
+            var p = Expression.Parameter(typeof(T));
+            var propertyReaderExp = Expression.PropertyOrField(p, propertyName);
+
+            return Expression.Lambda<Func<T, T2>>(propertyReaderExp, p);
+        }
+
+        /// <summary>
+        /// Invokes method <see cref="ForeignKeyTransformer.TransformForeignKey"/> on a property with a given name, that is resovled with a given expression
+        /// </summary>
+        /// <typeparam name="T">Type of the source object</typeparam>
+        /// <typeparam name="T2">Type of the property to be transformed</typeparam>
+        /// <param name="expr">Expression to resolve the property to be transformed</param>
+        /// <param name="propertyName">Name of the property to be transformed</param>
+        /// <returns></returns>
+        public static Expression<Func<T, T2>> GenerateForeignKeyTransformation<T, T2>(Expression<Func<T, T2>> expr, string propertyName)
+        {
+            var propertyNameExpression = Expression.Constant(propertyName);
+            var propertyValueExpressionParameter = Expression.Parameter(typeof(T));
+
+            var invokedExpression = Expression.Invoke(expr, propertyValueExpressionParameter);
+
+            var methodCallingExpression = Expression.Call(typeof(ForeignKeyTransformer).GetMethod(nameof(ForeignKeyTransformer.TransformForeignKey)).MakeGenericMethod(typeof(T2)), invokedExpression, propertyNameExpression);
+            return Expression.Lambda<Func<T, T2>>(methodCallingExpression, propertyValueExpressionParameter);
+        }
+    }
+}
